8bcad2ae9fac3f30c52626681adc46598a0a11d2
Samuel <samuelou510@gmail.com>
2024-09-06 19:37:34 -0400

    uncommented redirectot

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,10 +15,11 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  // if (redirectTo) {
-  //   return NextResponse.redirect(`${origin}${redirectTo}`);
-  // }
+  if (redirectTo) {
+    return NextResponse.redirect(`${origin}${redirectTo}`);
+  }
 
   // URL to redirect to after sign up process completes
   return NextResponse.redirect(`${origin}/verified`);
 }
+
