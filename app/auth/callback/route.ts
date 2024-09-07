22d3fccb9d1141bdf811aa972ba1d21dd1df243c
Samuel <samuelou510@gmail.com>
2024-09-07 13:01:13 -0400

    removed next response and change eto just next redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
-
+import { redirect } from "next/navigation";
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
@@ -8,18 +8,18 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
-  console.log(requestUrl, code, origin);
-
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
+
   if (code) {
     const supabase = createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
   if (redirectTo) {
-    return NextResponse.redirect(redirectTo);
+    return redirect(`${origin}${redirectTo}`);
   }
-
+  return redirect(`${origin}/verified`)
   // URL to redirect to after sign up process completes
   return NextResponse.redirect(`${origin}/verified`);
+  // return NextResponse.redirect(`${origin}/protected`);
 }
