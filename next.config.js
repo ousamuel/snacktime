79d2ff127524b6e990345dd86e0021285c1fc2f7
Samuel <samuelou510@gmail.com>
2024-09-25 15:52:49 -0400

    edit nextconfig.js to accomdate nextjs ssr + amplify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+  },
+};
 
 module.exports = nextConfig;
